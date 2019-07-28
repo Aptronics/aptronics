cd05083391b7dbe88b8fe388506a2b3e2c1843bc
Hemant Pema <hemant@pema.co.za>
2019-07-28 10:13:58 -0700

    Assessment Application

---
+++ aptronics/assessments/doctype/assessment_objective/assessment_objective.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Aptronics and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Assessment Objective', {
+	// refresh: function(frm) {
+
+	// }
+});
