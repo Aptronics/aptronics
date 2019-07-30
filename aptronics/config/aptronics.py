d1d8820db7f05e69829385f5e504472fa7f22859
Hemant Pema <hemant@pema.co.za>
2019-07-30 11:36:19 -0700

    Aptronics settings menu

--- aptronics/config/aptronics.py
+++ aptronics/config/aptronics.py
@@ -4,12 +4,23 @@ from frappe import _
 def get_data():
 	return [
 		{
-			"label": _("Assessment"),
+			"label": _("Settings"),
+			"icon": "fa fa-cog",
 			"items": [
 				{
 					"type": "doctype",
 					"name": "Assessment",
 					"description":_("SPORT.io assessment application")
+				}
+			]
+		},
+		{
+			"label": _("Assessment"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Assessment Feedback",
+					"description":_("SPORT.io assessment")
 				}
 			]
 		}
