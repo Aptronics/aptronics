deb3382cc20108cf115d3c083da5ff2d639171d1
Hemant Pema <hemant@pema.co.za>
2019-08-03 22:26:38 +0200

    get cost from batch

--- aptronics/stock/actual_cost.py
+++ aptronics/stock/actual_cost.py
@@ -7,6 +7,12 @@ def get_actual_cost_by_batch(doc, method):
     data = str(doc.as_dict())
     frappe.logger().info(doc.total_actual_cost)
     frappe.logger().info(method)
+    total_actual_cost = 0
     for i in doc.items:
         frappe.logger().info(i.batch_no)
-    
+        incoming_rate = frappe.db.sql("""select incoming_rate
+			from `tabStock Ledger Entry`
+			where batch_no = %s and item_code = %s""", (i.batch_no, i.item_code))
+		total_actual_cost = total_actual_cost + flt(incoming_rate[0][0])*i.qty
+
+    doc.total_actual_cost = total_actual_cost
