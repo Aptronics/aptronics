96cf37d389da3ad8fda8a00d5b1b861fd0e48453
Hemant Pema <hemant@pema.co.za>
2019-08-03 21:19:34 +0200

    get cost from batch

--- aptronics/stock/actual_cost.py
+++ aptronics/stock/actual_cost.py
@@ -5,5 +5,6 @@ import frappe
 @frappe.whitelist()
 def get_actual_cost_by_batch(doc, method):
     data = str(doc.as_dict())
-    frappe.logger().debug(doc.total_actual_cost)
+    frappe.logger().debug(data.total_actual_cost)
+    frappe.logger().debug(method)
     
