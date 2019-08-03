09fe5da050f00c5be14837bf68f681110fe6b51d
Hemant Pema <hemant@pema.co.za>
2019-08-03 21:49:56 +0200

    get cost from batch

--- aptronics/stock/actual_cost.py
+++ aptronics/stock/actual_cost.py
@@ -7,4 +7,6 @@ def get_actual_cost_by_batch(doc, method):
     data = str(doc.as_dict())
     frappe.logger().info(doc.total_actual_cost)
     frappe.logger().info(method)
+    for each i in doc.items:
+        frappe.logger().info(.description)
     
