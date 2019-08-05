81e72131168edd3bacea474a59cba2af59569ec2
Hemant Pema <hemant@pema.co.za>
2019-08-05 22:32:09 +0200

    hooks and logging for reversal journals

--- aptronics/hooks.py
+++ aptronics/hooks.py
@@ -85,10 +85,10 @@ doc_events = {
  	#	"on_submit": "aptronics.stock.actual_cost.reversal_shipment_cost_on_invoice",
 	#	"on_cancel": "aptronics.stock.actual_cost.reversal_shipment_cost_on_invoice_reversal",
 	},
-    "Stock Ledger Entry": {
- 		"after_insert": "aptronics.stock.actual_cost.accrue_shipment_cost",
-		"on_trash": "aptronics.stock.actual_cost.reversal_shipment_cost_on_shipment",
- 	}
+    #"Stock Ledger Entry": {
+ 	#	"after_insert": "aptronics.stock.actual_cost.accrue_shipment_cost",
+	#	"on_trash": "aptronics.stock.actual_cost.reversal_shipment_cost_on_shipment",
+ 	#}
  }
 
 # Scheduled Tasks
