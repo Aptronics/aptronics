629f7504ee4be13178ac65e79f07dfa5eb0a13da
Hemant Pema <hemant@pema.co.za>
2019-08-05 15:37:14 +0200

    formating file

--- aptronics/stock/actual_cost.py
+++ aptronics/stock/actual_cost.py
@@ -1,12 +1,11 @@
 from __future__ import unicode_literals
-
 import frappe
 
 @frappe.whitelist()
 def get_actual_cost_by_batch(doc, method):
-    #data = str(doc.as_dict())
+    # data = str(doc.as_dict())
     frappe.logger().info(doc.total_actual_cost)
-    #frappe.logger().info(method)
+    # frappe.logger().info(method)
     total_actual_cost = 0
     total_gross_profit = 0
     for i in doc.items:
@@ -15,9 +14,9 @@ def get_actual_cost_by_batch(doc, method):
 			where voucher_type = "Purchase Receipt" and 
             batch_no = %s and item_code = %s""", (i.batch_no, i.item_code))
         i.actual_cost = frappe.utils.flt(incoming_rate[0][0])
-        total_actual_cost = total_actual_cost + (i.actual_cost*i.qty)
+        total_actual_cost = total_actual_cost + (i.actual_cost * i.qty)
 
-        i.gross_profit = i.amount - (i.actual_cost*i.qty)
+        i.gross_profit = i.amount - (i.actual_cost * i.qty)
         total_gross_profit = total_gross_profit + (i.gross_profit)
 
     doc.total_actual_cost = total_actual_cost
