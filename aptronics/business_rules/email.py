205f5665eb859f4d5d6ee6123cfe3ce2f45b3a3a
Hemant Pema <hemant@pema.co.za>
2019-08-26 22:17:25 +0200

    added rules folder and email rules for demo and test period

--- aptronics/business_rules/email.py
+++ aptronics/business_rules/email.py
@@ -8,7 +8,27 @@ import frappe
 def check_email_address(doc, method):
     #data = str(doc.as_dict())
     frappe.logger().info(doc)
-    #for i in doc.recipients:
+    list = ''
+    domains = ['aptronics.co.za', 'eoh.com', 'eoh.co.za', 'ioco.tech']
+    for i in doc.recipients.split(","):
+        if i.split("@")[1] in domains:
+            list = list + i + ','
+
+    doc.recipients = list
+
+    list = ''
+    for i in doc.cc.split(","):
+        if i.split("@")[1] in domains:
+            list = list + i + ','
+
+    doc.cc = list
+
+    list = ''
+    for i in doc.bcc.split(","):
+        if i.split("@")[1] in domains:
+            list = list + i + ','
+
+    doc.bcc = list
     #    index = index + 1
     #    if not i.recipient.split("@")[1] in ['aptronics.co.za','eoh.com','eoh.co.za', 'ioco.tech']:
     #        frappe.logger().info('dont send for ' + i.recipient)
