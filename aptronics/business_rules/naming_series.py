6257ed7768135cd1506eb8948bba9d4fc68948bb
Hemant Pema <hemant@pema.co.za>
2019-09-27 00:06:31 +0200

    naming series for customer/suppliers

--- aptronics/business_rules/naming_series.py
+++ aptronics/business_rules/naming_series.py
@@ -6,7 +6,7 @@ import frappe
 import erpnext.setup.doctype.naming_series
 
 def business_partner_naming_series(doc, method):
-    frappe.logger().info(doc)
+    frappe.logger().info(doc.name)
     if doc.doctype=="Customer":
         doc.naming_series="C-" + doc.customer_name[:3].upper() + ".####"
 
