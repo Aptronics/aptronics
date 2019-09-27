9dc156b86c6e9d9b925d6a4195c768212aa04153
Hemant Pema <hemant@pema.co.za>
2019-09-27 08:10:51 +0200

    naming series for customer/suppliers

--- aptronics/business_rules/naming_series.py
+++ aptronics/business_rules/naming_series.py
@@ -13,3 +13,16 @@ def business_partner_naming_series(doc, method):
     if doc.doctype=="Supplier":
         doc.naming_series=doc.supplier_name[:3].upper() + ".####" #"S-" + doc.supplier_name[:3].upper() + ".####"
 
+
+@frappe.whitelist(allow_guest=False)
+def update_series(series, current_value):
+	if series:
+		prefix = series.split('.')[0]
+		if not frappe.db.exists('Series', series):
+			frappe.db.sql("insert into tabSeries (name, current) values (%s, 0)", (series))
+
+		frappe.db.sql("update `tabSeries` set current = %s where name = %s",
+			(current_value, prefix))
+		return True #(_("Series Updated Successfully"))
+	else:
+		return False #msgprint(_("Please select prefix first"))
