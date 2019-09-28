a7756ad7bd6278ded3346c1f11daadb0937f6123
Hemant Pema <hemant@pema.co.za>
2019-09-28 13:02:51 +0200

    naming series for customer/suppliers

--- aptronics/business_rules/naming_series.py
+++ aptronics/business_rules/naming_series.py
@@ -5,33 +5,47 @@ import sys
 import frappe
 import erpnext.setup.doctype.naming_series
 
-def business_partner_naming_series(doc, method):
-    #frappe.logger().info(doc.name)
-    if doc.doctype=="Customer":
-        doc.naming_series="C-" + doc.customer_name[:3].upper() + ".####" #doc.customer_name[:3].upper() + ".####" #
 
-    if doc.doctype=="Supplier":
-        doc.naming_series="S-" + doc.supplier_name[:3].upper() + ".####" # doc.supplier_name[:3].upper() + ".####" #
+def business_partner_naming_series(doc, method):
+    # frappe.logger().info(doc.name)
+    if doc.doctype == "Customer":
+        doc.naming_series = "C-" + doc.customer_name[:3].upper() + ".####"  # doc.customer_name[:3].upper() + ".####" #
+
+    if doc.doctype == "Supplier":
+        doc.naming_series = "S-" + doc.supplier_name[:3].upper() + ".####"  # doc.supplier_name[:3].upper() + ".####" #
 
 
 @frappe.whitelist(allow_guest=False)
 def update_series(series, current_value):
-	if series:
-		prefix = series.split('.')[0]
-		frappe.logger().info(str(prefix))
+    frappe.logger().info("series: " + series + " = " + str(current_value))
+    current = frappe.db.sql("SELECT `current` FROM `tabSeries` WHERE `name`=%s FOR UPDATE", (series,))
+    if current and current[0][0] is not None:
+        current = current[0][0]
+        frappe.logger().info("yes, update it")
+        frappe.db.sql("UPDATE `tabSeries` SET `current` = %s WHERE `name`=%s", (current_value, series))
+        current = cint(current) + 1
+    else:
+        frappe.logger().info("no, create it")
+        frappe.db.sql("INSERT INTO `tabSeries` (`name`, `current`) VALUES (%s, 1)", (series,))
+        current = 1
+    return ('%0' + str(digits) + 'd') % current
 
-		if not frappe.db.exists('Series', series):
-			frappe.logger().info("No series: " + str(series))
-			frappe.db.sql("insert into tabSeries (name, current) values (%s, 0)", (series))
-
-		frappe.db.sql("update `tabSeries` set current = %s where name = %s",
-			(current_value, series))
-		frappe.logger().info("Series Updated Successfully")
-		frappe.logger().info(str(series))
-		frappe.logger().info(str(current_value))
-		return True
-	else:
-		frappe.logger().info("Series Not Updated - Please select prefix first")
-		frappe.logger().info(str(series))
-		frappe.logger().info(str(current_value))
-		return False #msgprint(_("Please select prefix first"))
+    # if series:
+    #     prefix = series.split('.')[0]
+    #     frappe.logger().info(str(prefix))
+    #
+    #     if not frappe.db.exists('Series', series):
+    #         frappe.logger().info("No series: " + str(series))
+    #         frappe.db.sql("insert into tabSeries (name, current) values (%s, 0)", (series))
+    #
+    #     frappe.db.sql("update `tabSeries` set current = %s where name = %s",
+    #                   (current_value, series))
+    #     frappe.logger().info("Series Updated Successfully")
+    #     frappe.logger().info(str(series))
+    #     frappe.logger().info(str(current_value))
+    #     return True
+    # # else:
+    #     frappe.logger().info("Series Not Updated - Please select prefix first")
+    #     frappe.logger().info(str(series))
+    #     frappe.logger().info(str(current_value))
+    #     return False  # msgprint(_("Please select prefix first"))
