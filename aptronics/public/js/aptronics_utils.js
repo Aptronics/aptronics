668ecd9fcb7f5e189b1a6a826085428c8c32cd7a
Tyler <tyler@agritheory.com>
2019-10-22 09:42:17 -0400

    chore: remove link formatter for Buyer

--- aptronics/public/js/aptronics_utils.js
+++ aptronics/public/js/aptronics_utils.js
@@ -1,11 +1,3 @@
-frappe.form.link_formatters['User'] = (value, doc) => {
-	if(doc.buyer && doc.buyer_name) {
-		return doc.buyer_name + ': ' + doc.buyer;
-	} else {
-		return value;
-	}
-}
-
 function buyer_filter(frm){
 	frm.set_query("buyer", () => {
 		return {
