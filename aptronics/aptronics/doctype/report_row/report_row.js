6136fb3bbcfc23c862869dcf6b9011742243a7b6
Tyler Matteson <tyler@agritheory.com>
2020-02-18 14:42:02 -0500

    feat: Aptronics P&L with account numbers nad inline scripting

---
+++ aptronics/aptronics/doctype/report_row/report_row.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Aptronics and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Report Row', {
+	// refresh: function(frm) {
+
+	// }
+});
