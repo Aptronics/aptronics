90f48ef7a468ee3e4cce4eaad7f64ed6fdb140e4
Tyler Matteson <tyler@agritheory.com>
2020-02-19 12:02:01 -0500

    chore: remove doctype from aptronics (moved to frappe)

--- aptronics/aptronics/doctype/report_row/report_row.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2020, Aptronics and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Report Row', {
-	// refresh: function(frm) {
-
-	// }
-});
